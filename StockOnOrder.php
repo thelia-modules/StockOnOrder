badd3a43f528f381eebbfd50ed44d4872973efe9
Alexandre Nozière <anoziere@openstudio.fr>
2026-06-10 17:44:52 +0200

    fix: use __DIR__ instead of THELIA_MODULE_DIR in configureServices() exclude list

--- StockOnOrder.php
+++ StockOnOrder.php
@@ -80,7 +80,7 @@ class StockOnOrder extends BaseModule
     {
         $servicesConfigurator->load(self::getModuleCode().'\\', __DIR__)
             ->exclude([
-                THELIA_MODULE_DIR.ucfirst(self::getModuleCode()).'/I18n/*',
+                __DIR__.'/I18n/*',
                 __DIR__.'/Action/Base/',
                 __DIR__.'/Controller/Base/',
                 __DIR__.'/Form/Base/',
